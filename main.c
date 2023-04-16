7d2fe39c5ad898338ff60e6c751ddba3e2a7c629
AkhrievSuleym <114875962+AkhrievSuleym@users.noreply.github.com>
2023-04-16 18:12:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#define LEN 100
 
 typedef struct Node {
     int value; // Значение узла (элемента) списка
@@ -13,29 +15,27 @@ C_Node *push(C_Node *head, int number) {
     C_Node *temp = (C_Node*) malloc(sizeof(C_Node));
     temp->value = number; // Присваиваем ему значение
     temp->next = head; // Присваиваем указателю temp адрес предыдущего узла
-    head = temp; // Присваиваем узлу head адрес нового узла
     return temp;
 }
+
 
 // Задание множества
 C_Node *assignment(C_Node *head) {
     int number, count;
-    while (1) {
-        puts("Enter the number of elements:");
-        if (scanf("%d", &count) == 1) break;
+    puts("Enter the number of elements:");
+    while (!scanf("%d", &count)) {
         fflush(stdin);
         puts("ERROR! Count of numbers must be integer!");
+        puts("Enter the number of elements:");
     }
     for (int i = 0; i < count; i++) {
-        while (1) {
-            puts("Enter the element:");
-            if (scanf("%d", &number) == 1) {
-                head = push(head, number);
-                break;
-            }
+        puts("Enter the element:");
+        while (!scanf("%d", &number)) {
             fflush(stdin);
             puts("Numbers must be integer!");
+            puts("Enter the element:");
         }
+        head = push(head, number);
     }
     return head;
 }
@@ -45,29 +45,29 @@ void printList(C_Node *head) {
     while (head) {
         printf("%d", head->value);
         head = head->next;
-        printf("\n");
+        putchar('\n');
     }
 }
 
 // Перевод из 10СС в 2СС
-long long int convert(int number) {
-    long long int bin_result = 0, i = 1;
-    int remain;
-    while (number != 0) {
-        remain = number % 2;
-        number /= 2;
-        bin_result += remain * i;
-        i *= 10;
-        }
-    return bin_result;
+char* convert(int number) {
+    int i = 0;
+    char *bin_number = (char *)malloc(LEN * sizeof(char));
+    while (number > 0) {
+        bin_number[i++] = (number & 1) + '0';
+        number >>= 1;
+    }
+    bin_number[i] = '\0';
+    strrev(bin_number);
+    return bin_number;
 }
 
 int main() {
     C_Node* current_set = NULL;
     current_set = assignment(current_set);
     while (current_set) {
-        printf("%d - %lld\n", current_set->value, convert(current_set->value));
+        printf("%d - %s\n", current_set->value, convert(current_set->value));
         current_set = current_set->next;
-        }
+    }
     return 0;
 }
